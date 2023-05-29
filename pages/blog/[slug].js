93e58e58146c923d0dea9d599e0fb87b4243bafb
Hristijan Ilieski <ilieski.hristijan@gmail.com>
2023-05-29 23:02:25 +0200

    Strapi URL fix

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -27,7 +27,7 @@ export async function getServerSideProps({ params }) {
     `${process.env.STRAPI_ENDPOINT}/blog-articles?populate=*&filters[slug][$eq]=${params.slug}`,
     {
       headers: {
-        Authorization: `Bearer 54c1a5e9425d3b817a9f9610a3905fbbc881771a1e00bbb921ce9f95f890b84f8b2e46b2fdd3adb05e91f996d3cdae6bedbecfb42a95b65d925c8fb58edf34cbe6739720c9f289e11bf22e98bbf3cbd66b042678e2128c66132adaf9f930b9a7d03806623aeb51e1ea5105972160fa7d6b3d4474fdd801401e6251ad2675dc7e`,
+        Authorization: `Bearer ${process.env.STRAPI_TOKEN}`,
       },
     }
   );
@@ -35,20 +35,20 @@ export async function getServerSideProps({ params }) {
   const article = articles?.data[0];
 
   const tagsResponse = await fetch(
-    "${process.env.STRAPI_ENDPOINT}/blog-tags?populate=*",
+    `${process.env.STRAPI_ENDPOINT}/blog-tags?populate=*`,
     {
       headers: {
-        Authorization: `Bearer 54c1a5e9425d3b817a9f9610a3905fbbc881771a1e00bbb921ce9f95f890b84f8b2e46b2fdd3adb05e91f996d3cdae6bedbecfb42a95b65d925c8fb58edf34cbe6739720c9f289e11bf22e98bbf3cbd66b042678e2128c66132adaf9f930b9a7d03806623aeb51e1ea5105972160fa7d6b3d4474fdd801401e6251ad2675dc7e`,
+        Authorization: `Bearer ${process.env.STRAPI_TOKEN}`,
       },
     }
   );
   const tags = await tagsResponse.json();
 
   const recentArticlesResponse = await fetch(
-    "${process.env.STRAPI_ENDPOINT}/blog-articles?populate=*&pagination[limit]=3&sort=publishedOn:desc",
+    `${process.env.STRAPI_ENDPOINT}/blog-articles?populate=*&pagination[limit]=3&sort=publishedOn:desc`,
     {
       headers: {
-        Authorization: `Bearer 54c1a5e9425d3b817a9f9610a3905fbbc881771a1e00bbb921ce9f95f890b84f8b2e46b2fdd3adb05e91f996d3cdae6bedbecfb42a95b65d925c8fb58edf34cbe6739720c9f289e11bf22e98bbf3cbd66b042678e2128c66132adaf9f930b9a7d03806623aeb51e1ea5105972160fa7d6b3d4474fdd801401e6251ad2675dc7e`,
+        Authorization: `Bearer ${process.env.STRAPI_TOKEN}`,
       },
     }
   );
@@ -66,7 +66,7 @@ export async function getServerSideProps({ params }) {
     `${process.env.STRAPI_ENDPOINT}/blog-articles?populate=*&pagination[limit]=2&sort=publishedOn:desc${relatedArticleFilters}`,
     {
       headers: {
-        Authorization: `Bearer 54c1a5e9425d3b817a9f9610a3905fbbc881771a1e00bbb921ce9f95f890b84f8b2e46b2fdd3adb05e91f996d3cdae6bedbecfb42a95b65d925c8fb58edf34cbe6739720c9f289e11bf22e98bbf3cbd66b042678e2128c66132adaf9f930b9a7d03806623aeb51e1ea5105972160fa7d6b3d4474fdd801401e6251ad2675dc7e`,
+        Authorization: `Bearer ${process.env.STRAPI_TOKEN}`,
       },
     }
   );
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -30,40 +30,40 @@ const Blog = ({
 
 export async function getServerSideProps() {
   const featuredArticlesResponse = await fetch(
-    "${process.env.STRAPI_ENDPOINT}/blog-articles?populate=*&filters[isFeatured][$eq]=true",
+    `${process.env.STRAPI_ENDPOINT}/blog-articles?populate=*&filters[isFeatured][$eq]=true`,
     {
       headers: {
-        Authorization: `Bearer 54c1a5e9425d3b817a9f9610a3905fbbc881771a1e00bbb921ce9f95f890b84f8b2e46b2fdd3adb05e91f996d3cdae6bedbecfb42a95b65d925c8fb58edf34cbe6739720c9f289e11bf22e98bbf3cbd66b042678e2128c66132adaf9f930b9a7d03806623aeb51e1ea5105972160fa7d6b3d4474fdd801401e6251ad2675dc7e`,
+        Authorization: `Bearer ${process.env.STRAPI_TOKEN}`,
       },
     }
   );
   const featuredArticles = await featuredArticlesResponse.json();
 
   const popularArticlesResponse = await fetch(
-    "${process.env.STRAPI_ENDPOINT}/blog-articles?populate=*&filters[isPopular][$eq]=true&pagination[limit]=3&sort=publishedOn:desc",
+    `${process.env.STRAPI_ENDPOINT}/blog-articles?populate=*&filters[isPopular][$eq]=true&pagination[limit]=3&sort=publishedOn:desc`,
     {
       headers: {
-        Authorization: `Bearer 54c1a5e9425d3b817a9f9610a3905fbbc881771a1e00bbb921ce9f95f890b84f8b2e46b2fdd3adb05e91f996d3cdae6bedbecfb42a95b65d925c8fb58edf34cbe6739720c9f289e11bf22e98bbf3cbd66b042678e2128c66132adaf9f930b9a7d03806623aeb51e1ea5105972160fa7d6b3d4474fdd801401e6251ad2675dc7e`,
+        Authorization: `Bearer ${process.env.STRAPI_TOKEN}`,
       },
     }
   );
   const popularArticles = await popularArticlesResponse.json();
 
   const recentArticlesResponse = await fetch(
-    "${process.env.STRAPI_ENDPOINT}/blog-articles?populate=*&pagination[limit]=3&sort=publishedOn:desc",
+    `${process.env.STRAPI_ENDPOINT}/blog-articles?populate=*&pagination[limit]=3&sort=publishedOn:desc`,
     {
       headers: {
-        Authorization: `Bearer 54c1a5e9425d3b817a9f9610a3905fbbc881771a1e00bbb921ce9f95f890b84f8b2e46b2fdd3adb05e91f996d3cdae6bedbecfb42a95b65d925c8fb58edf34cbe6739720c9f289e11bf22e98bbf3cbd66b042678e2128c66132adaf9f930b9a7d03806623aeb51e1ea5105972160fa7d6b3d4474fdd801401e6251ad2675dc7e`,
+        Authorization: `Bearer ${process.env.STRAPI_TOKEN}`,
       },
     }
   );
   const recentArticles = await recentArticlesResponse.json();
 
   const allArticlesResponse = await fetch(
-    "${process.env.STRAPI_ENDPOINT}/blog-articles?populate=*&pagination[limit]=6&pagination[start]=3&sort=publishedOn:desc",
+    `${process.env.STRAPI_ENDPOINT}/blog-articles?populate=*&pagination[limit]=6&pagination[start]=3&sort=publishedOn:desc`,
     {
       headers: {
-        Authorization: `Bearer 54c1a5e9425d3b817a9f9610a3905fbbc881771a1e00bbb921ce9f95f890b84f8b2e46b2fdd3adb05e91f996d3cdae6bedbecfb42a95b65d925c8fb58edf34cbe6739720c9f289e11bf22e98bbf3cbd66b042678e2128c66132adaf9f930b9a7d03806623aeb51e1ea5105972160fa7d6b3d4474fdd801401e6251ad2675dc7e`,
+        Authorization: `Bearer ${process.env.STRAPI_TOKEN}`,
       },
     }
   );
