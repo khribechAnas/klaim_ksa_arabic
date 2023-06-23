9fc72f798680c075b21a7ef7958a28fed46e8769
Hristijan Ilieski <ilieski.hristijan@gmail.com>
2023-06-23 09:05:03 +0200

    Integrate hotjar.

--- pages/_app.js
+++ pages/_app.js
@@ -65,6 +65,17 @@ function MyApp({ Component, pageProps }) {
               data-domain="klaim.ai"
               src="https://plausible.klaim.ai/js/script.js"
             ></Script>
+            <Script id="hotjar" strategy="afterInteractive">
+              {`(function(h,o,t,j,a,r){
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:3546310,hjsv:6};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+              `}
+            </Script>
           </>
         )}
         <Component {...pageProps} />
