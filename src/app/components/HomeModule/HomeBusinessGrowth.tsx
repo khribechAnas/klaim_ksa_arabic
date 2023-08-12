ec0242de19dace897b33954c5a0c275f09547ef3
ViktorTodorovski92 <viktor.todorovski92@gmail.com>
2023-08-12 17:05:11 +0200

    Feature business growth

--- src/app/components/HomeModule/HomeBusinessGrowth.tsx
+++ src/app/components/HomeModule/HomeBusinessGrowth.tsx
@@ -2,36 +2,43 @@ import React from "react";
 
 const HomeBusinessGrowth = () => {
   return (
-    <section className="">
-      <div className="container mx-auto py-32">
-        <div className="bg-img-linear flex">
-          <div className="viktor-box flex-1">
-            <div className="px-8 py-7 flex items-center">
-              <img src="assets/images/Growth1.svg" />
-              <p className="text-white ml-8">
-                Stop losing revenue <br /> due to claim denials
-              </p>
-            </div>
-          </div>
-          <div className="w-8 bg-white" />
-          <div className="viktor-box flex-1">
-            <div className="px-8 py-7 flex items-center">
-              <img src="assets/images/Growth1.svg" />
-              <p className="text-white ml-8">
-                Stop losing revenue <br /> due to claim denials
-              </p>
-            </div>
-          </div>
-          <div className="w-8 bg-white" />
-          <div className="viktor-box flex-1">
-            <div className="px-8 py-7 flex items-center">
-              <img src="assets/images/Growth1.svg" />
-              <p className="text-white ml-8">
-                Reduce RCM-related <br /> staffing costs
-              </p>
-            </div>
+    <section className="container mx-auto pt-32 pb-36">
+      <h2 className="text-5xl font-semibold leading-[130%] text-[#3F465D]">
+        <span className="text-linear">Unleashing</span> <br />
+        business growth
+      </h2>
+      <div className="bg-img-linear flex my-8">
+        <div className="viktor-box flex-1">
+          <div className="px-8 py-7 flex items-center">
+            <img src="assets/images/Growth1.svg" />
+            <p className="text-white ml-8">
+              Stop losing revenue <br /> due to claim denials
+            </p>
           </div>
         </div>
+        <div className="w-8 bg-white" />
+        <div className="viktor-box flex-1">
+          <div className="px-8 py-7 flex items-center">
+            <img src="assets/images/Growth1.svg" />
+            <p className="text-white ml-8">
+              Stop losing revenue <br /> due to claim denials
+            </p>
+          </div>
+        </div>
+        <div className="w-8 bg-white" />
+        <div className="viktor-box flex-1">
+          <div className="px-8 py-7 flex items-center">
+            <img src="assets/images/Growth1.svg" />
+            <p className="text-white ml-8">
+              Reduce RCM-related <br /> staffing costs
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="pt-4">
+        <a className="bg-[#6135FB] text-white rounded-lg py-4 px-6 text-lg font-bold leading-4 tracking-tighter">
+          I’m interested!
+        </a>
       </div>
     </section>
   );
---
+++ src/app/components/HomeModule/HomeKlaimHelps.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HomeKlaimHelps = () => {
+  return (
+    <section className="py-16 text-center text-white bg-linear">
+      <p className="leading-10 font-bold text-[32px] mb-2">
+        Klaim helps you master the revenue cycle, boost profitability, and grow
+        faster
+      </p>
+      <p className="text-[28px] leading-9">starting in as little as 7 days.</p>
+    </section>
+  );
+};
+
+export default HomeKlaimHelps;
