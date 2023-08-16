df928206e9b861eedea57205ab40e6bf15578478
ViktorTodorovski92 <viktor.todorovski92@gmail.com>
2023-08-16 17:42:39 +0200

    Lightbox for videos

--- src/app/components/CommonModule/SlickSliderVideo.tsx
+++ src/app/components/CommonModule/SlickSliderVideo.tsx
@@ -1,9 +1,15 @@
 "use client";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import Slider from "react-slick";
+import { Lightbox } from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/plugins/captions.css";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
 
-const SlickSlider: React.FC = () => {
+const SlickSliderVideo: React.FC = () => {
   const sliderRef = useRef<Slider | null>(null);
+  const [showAboutUsVideoModal, setShowAboutUsVideoModal] = useState<
+    number | null
+  >(null);
 
   const next = () => {
     if (sliderRef.current) {
@@ -27,15 +33,39 @@ const SlickSlider: React.FC = () => {
 
   return (
     <div>
+      {/* <Lightbox
+        open={showAboutUsVideoModal !== null}
+        close={() => setShowAboutUsVideoModal(null)}
+        carousel={{ finite: true }}
+        styles={{ container: { backgroundColor: "rgba(26,42,58,0.94)" } }}
+        slides={videos}
+        render={{
+          slide: ({ slide }) => <>
+          <iframe
+            width="1000"
+            height="563"
+            src={showAboutUsVideoModal}
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+        </>,
+        }}
+      /> */}
       <Slider ref={(c) => (sliderRef.current = c)} {...settings}>
-        <div key={1} className="ml-36">
-          <div className="mx-2 pb-8 pt-12 px-6 bg-[#F5F7FB] rounded-lg"></div>
+        <div
+          key={1}
+          className="ml-36 cursor-pointer"
+          // onClick={() => setShowAboutUsVideoModal(true)}
+        >
+          <div className="mx-2 pb-8 pt-12 px-6 bg-[#F5F7FB] rounded-lg">1</div>
         </div>
         <div key={2} className="ml-36">
-          <div className="mx-2 pb-8 pt-12 px-6 bg-[#F5F7FB] rounded-lg"></div>
+          <div className="mx-2 pb-8 pt-12 px-6 bg-[#F5F7FB] rounded-lg">2</div>
         </div>
         <div key={3} className="ml-36">
-          <div className="mx-2 pb-8 pt-12 px-6 bg-[#F5F7FB] rounded-lg"></div>
+          <div className="mx-2 pb-8 pt-12 px-6 bg-[#F5F7FB] rounded-lg">3</div>
         </div>
       </Slider>
       <div className="mt-12 flex items-center justify-end">
@@ -50,4 +80,4 @@ const SlickSlider: React.FC = () => {
   );
 };
 
-export default SlickSlider;
+export default SlickSliderVideo;
