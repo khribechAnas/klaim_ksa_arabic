33792fe009132174350ebaedccfa1716d4db4510
ViktorTodorovski92 <viktor.todorovski92@gmail.com>
2023-09-03 12:35:45 +0200

    Hydration error fix

--- src/app/components/KapitalModule/KapitalTable.tsx
+++ src/app/components/KapitalModule/KapitalTable.tsx
@@ -1,7 +1,13 @@
-import React from "react";
-import ButtonPrimary from "../CommonModule/ButtonPrimary";
+"use client";
+import React, { useEffect, useState } from "react";
 
 const KapitalTable = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
     <section className="bg-[#3F465D]">
       <div className="container mx-auto py-12 md:py-32 px-4 md:px-0">
@@ -19,146 +25,148 @@ const KapitalTable = () => {
           <br />
           conventional claim financing
         </h2>
-        <div className="flex items-end my-8 overflow-x-auto">
-          <table className="bg-[#323748] text-white rounded-t-lg overflow-hidden w-[25%]">
-            <tr className="border-b-4 border-[#3F465D] h-[5rem] flex flex-col justify-center">
-              <td className="px-6 py-4 flex text-[1.25rem] md:text-[1.75rem] font-normal leading-[1.875rem] md:leading-[2.25rem]">
-                Model
-              </td>
-            </tr>
-            <tr className="border-b-4 border-[#3F465D] h-[6.5rem] md:h-[5rem] flex flex-col justify-center">
-              <td className="px-6 py-4 flex text-[1.25rem] md:text-[1.75rem] font-normal leading-[1.875rem] md:leading-[2.25rem]">
-                Commitment
-              </td>
-            </tr>
-            <tr className="border-b-4 border-[#3F465D] h-[8rem] md:h-[5rem] flex flex-col justify-center">
-              <td className="px-6 py-4 flex text-[1.25rem] md:text-[1.75rem] font-normal leading-[1.875rem] md:leading-[2.25rem]">
-                Costs
-              </td>
-            </tr>
-            <tr className="border-b-4 border-[#3F465D] h-[5rem] flex flex-col justify-center">
-              <td className="px-6 py-4 flex text-[1.25rem] md:text-[1.75rem] font-normal leading-[1.875rem] md:leading-[2.25rem]">
-                Payment timeline
-              </td>
-            </tr>
-            <tr className="border-b-4 border-[#3F465D] h-[5rem] flex flex-col justify-center">
-              <td className="px-6 py-4 flex text-[1.25rem] md:text-[1.75rem] font-normal leading-[1.875rem] md:leading-[2.25rem]">
-                Onboarding
-              </td>
-            </tr>
-            <tr className="border-b-4 border-[#3F465D] h-[5rem] flex flex-col justify-center">
-              <td className="px-6 py-4 flex text-[1.25rem] md:text-[1.75rem] font-normal leading-[1.875rem] md:leading-[2.25rem]">
-                Interests
-              </td>
-            </tr>
-          </table>
+        {isClient && (
+          <div className="flex items-end my-8 overflow-x-auto">
+            <table className="bg-[#323748] text-white rounded-t-lg overflow-hidden w-[25%]">
+              <tr className="border-b-4 border-[#3F465D] h-[5rem] flex flex-col justify-center">
+                <td className="px-6 py-4 flex text-[1.25rem] md:text-[1.75rem] font-normal leading-[1.875rem] md:leading-[2.25rem]">
+                  Model
+                </td>
+              </tr>
+              <tr className="border-b-4 border-[#3F465D] h-[6.5rem] md:h-[5rem] flex flex-col justify-center">
+                <td className="px-6 py-4 flex text-[1.25rem] md:text-[1.75rem] font-normal leading-[1.875rem] md:leading-[2.25rem]">
+                  Commitment
+                </td>
+              </tr>
+              <tr className="border-b-4 border-[#3F465D] h-[8rem] md:h-[5rem] flex flex-col justify-center">
+                <td className="px-6 py-4 flex text-[1.25rem] md:text-[1.75rem] font-normal leading-[1.875rem] md:leading-[2.25rem]">
+                  Costs
+                </td>
+              </tr>
+              <tr className="border-b-4 border-[#3F465D] h-[5rem] flex flex-col justify-center">
+                <td className="px-6 py-4 flex text-[1.25rem] md:text-[1.75rem] font-normal leading-[1.875rem] md:leading-[2.25rem]">
+                  Payment timeline
+                </td>
+              </tr>
+              <tr className="border-b-4 border-[#3F465D] h-[5rem] flex flex-col justify-center">
+                <td className="px-6 py-4 flex text-[1.25rem] md:text-[1.75rem] font-normal leading-[1.875rem] md:leading-[2.25rem]">
+                  Onboarding
+                </td>
+              </tr>
+              <tr className="border-b-4 border-[#3F465D] h-[5rem] flex flex-col justify-center">
+                <td className="px-6 py-4 flex text-[1.25rem] md:text-[1.75rem] font-normal leading-[1.875rem] md:leading-[2.25rem]">
+                  Interests
+                </td>
+              </tr>
+            </table>
 
-          {/*  */}
+            {/*  */}
 
-          <table className="bg-[#323748] text-white rounded-t-lg overflow-hidden mx-1 md:w-[37.5%]">
-            <tr className="border-b-4 border-[#3F465D] h-[4.25rem] flex flex-col justify-center w-[20rem] md:w-auto">
-              <td className="px-6 py-4 text-[1.25rem] md:text-[1.75rem] font-bold leading-[1.875rem] md:leading-[2.25rem]">
-                Conventional financing
-              </td>
-            </tr>
-            <tr className="border-b-4 border-[#3F465D] h-[5rem] flex flex-col justify-center">
-              <td className="px-6 py-4 flex text-[1rem] md:text-[1.125rem] font-semibold leading-[1.5rem]">
-                {" "}
-                <img src="assets/images/cross.svg" className="mr-6" /> Lending
-                money with fees and interest
-              </td>
-            </tr>
-            <tr className="border-b-4 border-[#3F465D] h-[6.5rem] md:h-[5rem] flex flex-col justify-center">
-              <td className="px-6 py-4 flex text-[1rem] md:text-[1.125rem] font-semibold leading-[1.5rem]">
-                {" "}
-                <img src="assets/images/cross.svg" className="mr-6" /> Long-term
-                commitment with complex entry <br /> and exit
-              </td>
-            </tr>
-            <tr className="border-b-4 border-[#3F465D] h-[8rem] md:h-[5rem] flex flex-col justify-center">
-              <td className="px-6 py-4 flex text-[1rem] md:text-[1.125rem] font-semibold leading-[1.5rem]">
-                {" "}
-                <img src="assets/images/cross.svg" className="mr-6" /> Interest
-                rate, recurring arrangement and <br /> processing fees, exit
-                fees and cancellation fees.
-              </td>
-            </tr>
-            <tr className="border-b-4 border-[#3F465D] h-[5rem] flex flex-col justify-center">
-              <td className="px-6 py-4 flex text-[1rem] md:text-[1.125rem] font-semibold leading-[1.5rem]">
-                {" "}
-                <img src="assets/images/cross.svg" className="mr-6" />{" "}
-                Open-ended
-              </td>
-            </tr>
-            <tr className="border-b-4 border-[#3F465D] h-[5rem] flex flex-col justify-center">
-              <td className="px-6 py-4 flex text-[1rem] md:text-[1.125rem] font-semibold leading-[1.5rem]">
-                {" "}
-                <img src="assets/images/cross.svg" className="mr-6" /> Weeks or
-                months
-              </td>
-            </tr>
-            <tr className="border-b-4 border-[#3F465D] h-[5rem] flex flex-col justify-center">
-              <td className="px-6 py-4 flex text-[1rem] md:text-[1.125rem] font-semibold leading-[1.5rem]">
-                {" "}
-                <img src="assets/images/cross.svg" className="mr-6" />
-                Often unpredictable
-              </td>
-            </tr>
-          </table>
+            <table className="bg-[#323748] text-white rounded-t-lg overflow-hidden mx-1 md:w-[37.5%]">
+              <tr className="border-b-4 border-[#3F465D] h-[4.25rem] flex flex-col justify-center w-[20rem] md:w-auto">
+                <td className="px-6 py-4 text-[1.25rem] md:text-[1.75rem] font-bold leading-[1.875rem] md:leading-[2.25rem]">
+                  Conventional financing
+                </td>
+              </tr>
+              <tr className="border-b-4 border-[#3F465D] h-[5rem] flex flex-col justify-center">
+                <td className="px-6 py-4 flex text-[1rem] md:text-[1.125rem] font-semibold leading-[1.5rem]">
+                  {" "}
+                  <img src="assets/images/cross.svg" className="mr-6" /> Lending
+                  money with fees and interest
+                </td>
+              </tr>
+              <tr className="border-b-4 border-[#3F465D] h-[6.5rem] md:h-[5rem] flex flex-col justify-center">
+                <td className="px-6 py-4 flex text-[1rem] md:text-[1.125rem] font-semibold leading-[1.5rem]">
+                  {" "}
+                  <img src="assets/images/cross.svg" className="mr-6" />{" "}
+                  Long-term commitment with complex entry <br /> and exit
+                </td>
+              </tr>
+              <tr className="border-b-4 border-[#3F465D] h-[8rem] md:h-[5rem] flex flex-col justify-center">
+                <td className="px-6 py-4 flex text-[1rem] md:text-[1.125rem] font-semibold leading-[1.5rem]">
+                  {" "}
+                  <img src="assets/images/cross.svg" className="mr-6" />{" "}
+                  Interest rate, recurring arrangement and <br /> processing
+                  fees, exit fees and cancellation fees.
+                </td>
+              </tr>
+              <tr className="border-b-4 border-[#3F465D] h-[5rem] flex flex-col justify-center">
+                <td className="px-6 py-4 flex text-[1rem] md:text-[1.125rem] font-semibold leading-[1.5rem]">
+                  {" "}
+                  <img src="assets/images/cross.svg" className="mr-6" />{" "}
+                  Open-ended
+                </td>
+              </tr>
+              <tr className="border-b-4 border-[#3F465D] h-[5rem] flex flex-col justify-center">
+                <td className="px-6 py-4 flex text-[1rem] md:text-[1.125rem] font-semibold leading-[1.5rem]">
+                  {" "}
+                  <img src="assets/images/cross.svg" className="mr-6" /> Weeks
+                  or months
+                </td>
+              </tr>
+              <tr className="border-b-4 border-[#3F465D] h-[5rem] flex flex-col justify-center">
+                <td className="px-6 py-4 flex text-[1rem] md:text-[1.125rem] font-semibold leading-[1.5rem]">
+                  {" "}
+                  <img src="assets/images/cross.svg" className="mr-6" />
+                  Often unpredictable
+                </td>
+              </tr>
+            </table>
 
-          {/*  */}
+            {/*  */}
 
-          <table className="bg-linear-table text-white rounded-t-lg overflow-hidden w-[37.5%]">
-            <tr className="border-b-4 border-[#3F465D] h-[4.25rem] flex flex-col justify-center w-[20rem] md:w-auto">
-              <td className="px-6 py-4 text-[1.25rem] md:text-[1.75rem] font-bold leading-[1.875rem] md:leading-[2.25rem]">
-                Klaim Kapital
-              </td>
-            </tr>
-            <tr className="border-b-4 border-[#3F465D] h-[5rem] flex flex-col justify-center">
-              <td className="px-6 py-4 flex text-[1rem] md:text-[1.125rem] font-semibold leading-[1.5rem]">
-                {" "}
-                <img src="assets/images/check.svg" className="mr-6" />{" "}
-                Purchasing claims as assets
-              </td>
-            </tr>
-            <tr className="border-b-4 border-[#3F465D] h-[6.5rem] md:h-[5rem] flex flex-col justify-center">
-              <td className="px-6 py-4 flex text-[1rem] md:text-[1.125rem] font-semibold leading-[1.5rem]">
-                {" "}
-                <img src="assets/images/check.svg" className="mr-6" /> No
-                commitment with easy entry and exit
-              </td>
-            </tr>
-            <tr className="border-b-4 border-[#3F465D] h-[8rem] md:h-[5rem] flex flex-col justify-center">
-              <td className="px-6 py-4 flex text-[1rem] md:text-[1.125rem] font-semibold leading-[1.5rem]">
-                {" "}
-                <img src="assets/images/check.svg" className="mr-6" /> Minimal
-                one-time admin/legal fee and a claim{" "}
-                <br className="hidden md:block" /> value percentage only
-              </td>
-            </tr>
-            <tr className="border-b-4 border-[#3F465D] h-[5rem] flex flex-col justify-center">
-              <td className="px-6 py-4 flex text-[1rem] md:text-[1.125rem] font-semibold leading-[1.5rem]">
-                {" "}
-                <img src="assets/images/check.svg" className="mr-6" /> 24 hours
-                after claim purchase
-              </td>
-            </tr>
-            <tr className="border-b-4 border-[#3F465D] h-[5rem] flex flex-col justify-center">
-              <td className="px-6 py-4 flex text-[1rem] md:text-[1.125rem] font-semibold leading-[1.5rem]">
-                {" "}
-                <img src="assets/images/check.svg" className="mr-6" /> As little
-                as 5-7 business days
-              </td>
-            </tr>
-            <tr className="border-b-4 border-[#3F465D] h-[5rem] flex flex-col justify-center">
-              <td className="px-6 py-4 flex text-[1rem] md:text-[1.125rem] font-semibold leading-[1.5rem]">
-                {" "}
-                <img src="assets/images/check.svg" className="mr-6" /> No
-                interests
-              </td>
-            </tr>
-          </table>
-        </div>
+            <table className="bg-linear-table text-white rounded-t-lg overflow-hidden w-[37.5%]">
+              <tr className="border-b-4 border-[#3F465D] h-[4.25rem] flex flex-col justify-center w-[20rem] md:w-auto">
+                <td className="px-6 py-4 text-[1.25rem] md:text-[1.75rem] font-bold leading-[1.875rem] md:leading-[2.25rem]">
+                  Klaim Kapital
+                </td>
+              </tr>
+              <tr className="border-b-4 border-[#3F465D] h-[5rem] flex flex-col justify-center">
+                <td className="px-6 py-4 flex text-[1rem] md:text-[1.125rem] font-semibold leading-[1.5rem]">
+                  {" "}
+                  <img src="assets/images/check.svg" className="mr-6" />{" "}
+                  Purchasing claims as assets
+                </td>
+              </tr>
+              <tr className="border-b-4 border-[#3F465D] h-[6.5rem] md:h-[5rem] flex flex-col justify-center">
+                <td className="px-6 py-4 flex text-[1rem] md:text-[1.125rem] font-semibold leading-[1.5rem]">
+                  {" "}
+                  <img src="assets/images/check.svg" className="mr-6" /> No
+                  commitment with easy entry and exit
+                </td>
+              </tr>
+              <tr className="border-b-4 border-[#3F465D] h-[8rem] md:h-[5rem] flex flex-col justify-center">
+                <td className="px-6 py-4 flex text-[1rem] md:text-[1.125rem] font-semibold leading-[1.5rem]">
+                  {" "}
+                  <img src="assets/images/check.svg" className="mr-6" /> Minimal
+                  one-time admin/legal fee and a claim{" "}
+                  <br className="hidden md:block" /> value percentage only
+                </td>
+              </tr>
+              <tr className="border-b-4 border-[#3F465D] h-[5rem] flex flex-col justify-center">
+                <td className="px-6 py-4 flex text-[1rem] md:text-[1.125rem] font-semibold leading-[1.5rem]">
+                  {" "}
+                  <img src="assets/images/check.svg" className="mr-6" /> 24
+                  hours after claim purchase
+                </td>
+              </tr>
+              <tr className="border-b-4 border-[#3F465D] h-[5rem] flex flex-col justify-center">
+                <td className="px-6 py-4 flex text-[1rem] md:text-[1.125rem] font-semibold leading-[1.5rem]">
+                  {" "}
+                  <img src="assets/images/check.svg" className="mr-6" /> As
+                  little as 5-7 business days
+                </td>
+              </tr>
+              <tr className="border-b-4 border-[#3F465D] h-[5rem] flex flex-col justify-center">
+                <td className="px-6 py-4 flex text-[1rem] md:text-[1.125rem] font-semibold leading-[1.5rem]">
+                  {" "}
+                  <img src="assets/images/check.svg" className="mr-6" /> No
+                  interests
+                </td>
+              </tr>
+            </table>
+          </div>
+        )}
         <div className="text-center py-4">
           <a className="bg-[#F5F7FB] text-[#6135FB] rounded-lg py-4 px-6 text-sm md:text-lg font-bold leading-4 tracking-[0.0225rem]">
             I&#39;m interested
