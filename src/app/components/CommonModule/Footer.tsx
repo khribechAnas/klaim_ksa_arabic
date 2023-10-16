3f6153f3ea43931b985bc49026eed2ee4eec70d5
ViktorTodorovski92 <viktor.todorovski92@gmail.com>
2023-10-16 12:37:01 +0200

    Footer changes address

--- src/app/components/CommonModule/Footer.tsx
+++ src/app/components/CommonModule/Footer.tsx
@@ -15,30 +15,49 @@ const Footer = () => {
               <a href="/about">About Us</a>
             </div>
           </div>
-          <div className="flex flex-col xl:flex-row items-start xl:items-center justify-between my-8 xl:my-16">
+          <div className="flex mt-8 xl:mt-16">
+            <a
+              href="https://www.linkedin.com/company/klaim-ai/"
+              target="_blank"
+            >
+              <img src="assets/images/linkedin.svg" className="mr-4" />
+            </a>
+            <a
+              href="https://www.youtube.com/channel/UCSFyygyt3hE05B9nGNCqHMA"
+              target="_blank"
+            >
+              <img src="assets/images/youtube.svg" className="mr-4" />
+            </a>
+            <a href="https://www.twitter.com/AiKlaim" target="_blank">
+              <img src="assets/images/twitter.svg" className="mr-4" />
+            </a>
+            <a href="https://www.instagram.com/klaim.ai/" target="_blank">
+              <img src="assets/images/instagram.svg" className="mr-4" />
+            </a>
+
+            <a
+              href="https://www.facebook.com/profile.php?id=100063848329885"
+              target="_blank"
+            >
+              <img src="assets/images/facebook.svg" className="mr-4" />
+            </a>
+          </div>
+          <div className="flex flex-col xl:flex-row items-start xl:items-start justify-between my-8 xl:my-16">
             <div className="mr-4 mb-8 xl:mb-0">
-              <div className="flex mb-8">
-                <img src="assets/images/youtube.svg" className="mr-4" />
-                <img src="assets/images/twitter.svg" className="mr-4" />
-                <img src="assets/images/instagram.svg" className="mr-4" />
-                <img src="assets/images/linkedin.svg" className="mr-4" />
-                <img src="assets/images/viber.svg" className="mr-4" />
-                <img src="assets/images/facebook.svg" className="mr-4" />
-              </div>
-              <h2 className="text-white font-bold leading-[16px] md:leading-4 text-sm md:text-lg tracking-tight mb-4">
+              <h2 className="text-white font-bold leading-[16px] md:leading-4 text-sm md:text-lg tracking-wider mb-4">
                 Newsletter
               </h2>
-              <p className="text-white text-sm md:text-base leading-[25px] md:leading-none font-normal mb-4">
+              <p className="text-white text-sm md:text-base leading-[25px] font-normal mb-4">
                 Join the Klaim community for the latest product <br /> updates,
                 industry insights, and exciting news. <br />
                 Subscribe now to be in the know!
               </p>
               <FooterNewsletter />
             </div>
-            <div className="flex flex-col md:flex-row">
-              <div className="text-white text-sm md:text-base leading-[25px] md:leading-none font-normal">
-                <p className="mb-2">Al Khatem Tower 4, ADGM Square,</p>
-                <p className="mb-2">Abu Dhabi, UAE,</p>
+            <div className="grid grid-cols-2 gap-x-8 md:gap-x-20 gap-y-8 md:gap-y-12">
+              <div className="text-white text-right text-sm md:text-base leading-[25px] md:leading-none font-normal ">
+                <p className="mb-4 font-bold">Dubai, UAE</p>
+                <p className="mb-2">3404 Indigo Icon Tower, JLT Cluster F</p>
                 <p className="mb-2">
                   <a href="mailto:hello@klaim.ai">hello@klaim.ai</a>
                 </p>
@@ -46,24 +65,34 @@ const Footer = () => {
                   <a href="tel:+97148764096">+971 4 876 4096</a>
                 </p>
               </div>
-              <div className="text-white text-sm md:text-base leading-[25px] md:leading-none font-normal mx-0 md:mx-16 my-8 md:my-0">
-                <p className="mb-2">Indigo Icon Tower 3404, JLT Cluster F,</p>
-                <p className="mb-2">Dubai, UAE</p>
-                <p className="mb-2">
-                  <a href="mailto:hello@klaim.ai">hello@klaim.ai</a>
-                </p>
-                <p>
-                  <a href="tel:+97148764096">+971 4 876 4096</a>
-                </p>
-              </div>
-              <div className="text-white text-sm md:text-base leading-[25px] md:leading-none font-normal">
-                <p className="mb-2">National address RRMA3141, Riyadh,</p>
-                <p className="mb-2">Kingdom of Saudi Arabia</p>
+              <div className="text-white text-right text-sm md:text-base leading-[25px] md:leading-none font-normal">
+                <p className="mb-4 font-bold">Riyadh, KSA</p>
+                <p className="mb-2">3141 Anas Ibn Malik Rd, Al Malqa</p>
                 <p className="mb-2">
                   <a href="mailto:helloksa@klaim.ai">helloksa@klaim.ai</a>
                 </p>
                 <p>
                   <a href="tel:+966115102972">+966 11 510 2972</a>
+                </p>
+              </div>
+              <div className="text-white text-right text-sm md:text-base leading-[25px] md:leading-none font-normal">
+                <p className="mb-4 font-bold">Abu Dhabi, UAE</p>
+                <p className="mb-2">Al Khatem Tower 4, ADGM Square</p>
+                <p className="mb-2">
+                  <a href="mailto:hello@klaim.ai">hello@klaim.ai</a>
+                </p>
+                <p>
+                  <a href="tel:+97148764096">+971 4 876 4096</a>
+                </p>
+              </div>
+              <div className="text-white text-right text-sm md:text-base leading-[25px] md:leading-none font-normal">
+                <p className="mb-4 font-bold">Muscat, Oman</p>
+                <p className="mb-2">395 Tamimah Building, Al Nahdah Road</p>
+                <p className="mb-2">
+                  <a href="mailto:helloksa@klaim.ai">hellooman@klaim.ai</a>
+                </p>
+                <p>
+                  <a href="tel:+96871734424">+968 7 173 4424</a>
                 </p>
               </div>
             </div>
