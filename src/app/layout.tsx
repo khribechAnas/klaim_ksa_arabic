b95b12eb457889c7f3f339b8131706fe74cb1146
Viktor Todorovski <viktor.todorovski92@gmail.com>
2024-09-23 14:49:32 +0200

    Add id scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         {process.env.NODE_ENV === "production" && (
-          <Script>
+          <Script id="googleTagManager" strategy="afterInteractive">
             {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
