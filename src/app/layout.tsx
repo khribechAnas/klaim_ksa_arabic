5f1a7e00445f3f02aabf6700028c3e44305574ec
gligorsimonov <gligor.simonov@tgn.mk>
2025-07-25 13:44:08 +0200

    Change import spacing.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import Script from "next/script";
-import {PostHogProvider} from "@/app/providers/posthogProvider";
+import { PostHogProvider } from "@/app/providers/posthogProvider";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
