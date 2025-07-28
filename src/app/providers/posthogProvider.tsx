f5c0aea159189908b9ffc0f8d9070ed6b8b0a62d
gligorsimonov <gligor.simonov@tgn.mk>
2025-07-28 11:47:49 +0200

    Change indent.

--- src/app/providers/posthogProvider.tsx
+++ src/app/providers/posthogProvider.tsx
@@ -4,11 +4,11 @@ import { PostHogProvider as PHProvider } from 'posthog-js/react'
 import posthog from "@/utils/posthog";
 
 const PostHogProvider = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <PHProvider client={posthog}>
-            {children}
-        </PHProvider>
-    );
+  return (
+    <PHProvider client={posthog}>
+      {children}
+    </PHProvider>
+  );
 };
 
 export default PostHogProvider; // ✅ default export
